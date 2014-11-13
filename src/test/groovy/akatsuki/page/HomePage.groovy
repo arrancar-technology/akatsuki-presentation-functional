a0fd033589463f98e8efe0e34a405599d1d28655
Deniz KALFA <dkalfa@gmail.com>
2014-11-13 19:51:00 +0000

    @thyms: updated title, meta-description and h1 of homepage.

--- src/test/groovy/akatsuki/page/HomePage.groovy
+++ src/test/groovy/akatsuki/page/HomePage.groovy
@@ -1,16 +1,15 @@
 package akatsuki.page
 
-import akatsuki.page.module.PanelApplyFormModule
 import akatsuki.path.PathPresentation
 
 class HomePage extends BasePage {
 
   static url = PathPresentation.BASE
-  static at = { title == "Simply Certificate" }
+  static at = { title == "Welcome to Simply Certificate" }
 
-  static content = {
-    panelBirth { module PanelApplyFormModule, $('#birth-section'), certificateType: 'birth' }
+//  static content = {
+//    panelBirth { module PanelApplyFormModule, $('#birth-section'), certificateType: 'birth' }
 //    panelMarriage { module PanelApplyFormModule, $('#marriage-section'), certificateType: 'marriage' }
 //    panelDeath { module PanelApplyFormModule, $('#death-section'), certificateType: 'death' }
-  }
+//  }
 }
